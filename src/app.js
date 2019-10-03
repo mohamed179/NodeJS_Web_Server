cc130ecdcdccf2aa8cf2a8fd4fc1389c3f1cfe8d
mohamed179 <mohamed77zaki@gmail.com>
2019-10-03 23:46:22 +0200

    Section 7 Step 3

--- src/app.js
+++ src/app.js
@@ -3,19 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello world!');
+  res.send('<h1>Hello world!</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help');
+  res.send([
+    {
+      name: 'Mohamed',
+      age: 25
+    },
+    {
+      name: 'Ahmed',
+      age: 27
+    }
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About');
+  res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather');
+  res.send({
+  	location: 'Alexandria',
+  	forecast: 'It is sunny'
+  });
 });
 
 app.listen('3000', () => {
